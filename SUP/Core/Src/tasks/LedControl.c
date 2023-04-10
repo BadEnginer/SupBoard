b3853d770791d591abe0c63b554d2f5510211a41
BadEnginer <badenginer@gmail.com>
2023-04-10 21:13:00 +0900

    Добавил немного диодов в задачу.

--- SUP/Core/Src/tasks/LedControl.c
+++ SUP/Core/Src/tasks/LedControl.c
@@ -5,8 +5,15 @@ void StartLedControlTask(void *argument){
 	  ARGB_Init();  // Initialization
 	  ARGB_Clear();
 	  while (ARGB_Show() != ARGB_OK);
-	  ARGB_SetBrightness(80);
+	  ARGB_SetBrightness(100);
+	  ARGB_Clear(); // Clear stirp
+	  while (ARGB_Show() != ARGB_OK);
+
 	for(;;){
-		osDelay(100);
+		ARGB_SetRGB(1, 0, 50, 0); // Set LED №1 with 255 Green
+		ARGB_SetRGB(2, 50, 0, 0); // Set LED №2 with 255 Green
+		ARGB_SetRGB(3, 0, 0, 50); // Set LED №3 with 255 Green
+		while (ARGB_Show() != ARGB_OK);
+		osDelay(25);
 		}
 }
